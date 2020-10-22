c91245c8ec393fa85b5115a17b434ca66b437338
Jakob Persons <jakobep24@gmail.com>
2020-10-22 15:20:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = ""
-  passengers.each do |key, value|
-    if key == :suite_a && value.start_with?("A")
-      winner = value
+  winner = "" # setting variable winner equal to an empty string
+  passengers.each do |key, value| # enumerate over given array using each and giving both key and the value of the key as arguments
+    if key == :suite_a && value.start_with?("A") # if key is equal to :suite_a and the value it points to start with a capital A
+      winner = value # set that value equal to the winner variable
     end
   end
-  p winner
+  p winner # p the winner variable
 end
