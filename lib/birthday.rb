7a5f41b6f809b9a88b40bed484a24518526d4f7c
Jakob Persons <jakobep24@gmail.com>
2020-10-22 15:23:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  birthday_kids.each do |name, age| # enumerate over given array using each, giving key/value pair as an argument
+    puts "Happy Birthday #{name}! You are now #{age} years old!" # puts out phrase interpolating the name(key)/age(value) pair
   end
 end
